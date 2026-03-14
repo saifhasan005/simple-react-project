7bd1bc0bca4dd5b26377b58d2bfcf9dd484df9e1
Saif205-svg <rafiuzzamansaif005@gmail.com>
2026-03-14 22:24:54 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Countries from './Componenrs/countries/Countries'
+
+function App() {
+
+  return (
+    <>
+    <Countries></Countries>
+    </>
+  )
+}
+
+export default App
---
+++ src/Componenrs/countries/Countries.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Countries = () => {
+    return (
+        <div>
+            <h3>Hello world</h3>
+        </div>
+    );
+};
+
+export default Countries;
